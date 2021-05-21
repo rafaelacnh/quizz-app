073c11ec53e46a60ebda8fec5656c690cb141f56
rafaela <rafaela.cunha@calindra.com.br>
2021-05-20 23:23:05 -0300

    iniciando projeto add perguntas e estrutura html

---
+++ script.js
@@ -0,0 +1,26 @@
+const quizData = [
+    {
+        question: 'What is my favorit color',
+        a: 'Black',
+        b: 'White',
+        c: 'Red',
+        d: 'Green',
+        correct: 'c'
+    },
+    {
+        question: 'How many dogs i have?',
+        a: '1',
+        b: '0',
+        c: '2',
+        d: '3',
+        correct: 'd'
+    },
+    {
+        question: 'What is my favorite singer?',
+        a: 'Beyoncé',
+        b: 'Mariah Carey',
+        c: 'Riannha',
+        d: 'Ariana Grande',
+        correct: 'a'
+    }
+]
